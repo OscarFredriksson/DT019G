afd45c38d2e51c32d5a9e87d331cade70423edcc
Oscar <osfr1701@student.miun.se>
2018-04-21 11:41:25 +0200

    Skapade en headerfil för den länkade listan

---
+++ Labb3/linked_list.h
@@ -0,0 +1,57 @@
+#include <iostream>
+
+class linked_list 
+{
+public :
+    linked_list ();
+    
+    linked_list ( const linked_list & src );
+    
+    ~ linked_list ();
+    
+    linked_list & operator =( const linked_list & rhs );
+    
+    // adds elements to the back .
+    linked_list & operator +=( const linked_list & rhs );
+    
+    // inserting elements
+    void insert ( double value , size_t pos );
+    
+    // in front
+    void push_front ( double value );
+    
+    // in back
+    void push_back ( double value );
+    
+    // accessing elements
+    double front() const;
+    double back() const;
+    double at (size_t pos) const;
+    
+    // removing elements
+    void remove ( size_t pos );
+    
+    // remove and access
+    double pop_front ();
+    double pop_back ();
+    
+    // informational
+    size_t size () const;
+    bool is_empty () const;
+    
+    // output
+    void print () const;
+    void print_reverse () const;
+        
+private :
+    struct node_t 
+    {
+        node_t ( double value , node_t * next =0 , node_t * prev =0) ;
+        double value ;
+        node_t * next ;
+        node_t * prev ;
+    };
+
+    node_t * head ;
+    node_t * tail ;
+};
